4e758d8a5d50899f7b5a5153a5feb4ecca174336
usernamekshitij <usernamekshitij@gmail.com>
2019-06-01 14:19:46 +0530

    changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -61,7 +61,7 @@ import os.path
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'/blog/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'django_project/blog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
