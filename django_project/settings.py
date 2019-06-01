0243c21f7c43ecd3625f94a17102af38e58e6c2f
usernamekshitij <usernamekshitij@gmail.com>
2019-06-01 12:45:16 +0530

    ADDED POSTGRES

--- django_project/settings.py
+++ django_project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '/templates/blog')],
+        'DIRS': [os.path.join(BASE_DIR, 'blog/templates/blog')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
