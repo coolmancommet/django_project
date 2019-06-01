6839813f052bd2dc8553074b399b4468b53167e5
usernamekshitij <usernamekshitij@gmail.com>
2019-06-01 12:26:56 +0530

    ADDED POSTGRES

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sqtja$+gjg8&dti+tzce%8+gi=1nzz1bz0$y=cx*)&g=@0wn=&'
+#SECRET_KEY = 'sqtja$+gjg8&dti+tzce%8+gi=1nzz1bz0$y=cx*)&g=@0wn=&'
 SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
